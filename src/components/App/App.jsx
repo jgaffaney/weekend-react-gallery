3a990426b7f4ad5a073dc52538c77468ea0b67d0
John Gaffaney <87045628+jgaffaney@users.noreply.github.com>
2021-10-21 18:17:40 -0500

    basic setup, GET route completed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+
+    useEffect(() => {
+        fetchPics();
+    }, []);
+
+    const fetchPics = () => {
+      console.log('in fetchPics');
+      axios.get('/gallery')
+          .then(response => {
+              console.log('Axios response: ', response);
+              setGallery(response.data)
+          })
+          .catch(err => {
+              console.log('Axios error: ', err);
+          })
+    }
+
     return (
       <div className="App">
         <header className="App-header">
