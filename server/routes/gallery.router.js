6f047ed01ba67a10adb6328b37d61c5094f60183
John Gaffaney <87045628+jgaffaney@users.noreply.github.com>
2021-10-23 11:31:01 -0500

    pg installed, routes updated to db

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const galleryItems = require('../modules/gallery.data');
+// const galleryItems = require('../modules/gallery.data');
+const pool = require('../modules/pool.js')
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
@@ -9,18 +10,32 @@ router.put('/like/:id', (req, res) => {
     console.log(req.params);
     const galleryId = req.params.id;
 
-    const queryText = `UPDATE `
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
+    const queryText = `UPDATE gallery SET likes=likes+1 WHERE id=$1;`;
+    const values = [galleryId]
+
+    pool.query(queryText, values)
+        .then(response => {
+            console.log('response from DB: ', response);
+            res.sendStatus(200);
+        })
+        .catch(err => {
+            console.log('Error from DB: ', err);
+            res.sendStatus(500)
+        })
 }); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
-    res.send(galleryItems);
-}); // END GET Route
+    const queryText = `SELECT * FROM gallery ORDER BY id;`
+    pool.query(queryText)
+        .then(response => {
+            console.log('Response from DB: ', response)
+            res.send(response.rows)
+        }).catch(err => {
+            console.log('Error on GET from DB: ', err);
+            res.sendStatus(500);
+        });
+});
+         // END GET Route
 
 module.exports = router;
