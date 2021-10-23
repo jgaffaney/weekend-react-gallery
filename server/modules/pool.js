4aabaca315b4d7785e3dba50c6ce76c519cc68fe
John Gaffaney <87045628+jgaffaney@users.noreply.github.com>
2021-10-23 10:33:06 -0500

    pool created and connected

---
+++ server/modules/pool.js
@@ -0,0 +1,21 @@
+const pg = require('pg');
+
+const Pool = pg.Pool;
+
+const pool = new Pool({
+    database: 'react-gallery',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+});
+
+pool.on('connect', () => {
+    console.log('Connected to DB');
+});
+
+pool.on('error', () => {
+    console.log('Error connecting to DB');    
+});
+
+module.exports = pool;
